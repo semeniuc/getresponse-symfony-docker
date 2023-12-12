4406a78161064095135e52c498b46fcae4430ebb
Vadim Semeniuc <semeniuc@int.pl>
2023-12-12 17:07:30 +0100

    create collection_controller

--- app/assets/controllers/collection_controller.js
+++ app/assets/controllers/collection_controller.js
@@ -3,16 +3,43 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ['type', 'stage']
 
+    static values = {
+        index    : Number,
+        prototype: String,
+    }
+
     add(event) {
         event.preventDefault();
+        
+        // Generation data
+        const generation =  document.createElement('section');
+        generation.innerHTML = this.prototypeValue.replace(/__name__/g, this.indexValue);
+        
+        // Get data
+        const data = Array.from(generation.querySelectorAll('div > div'));
 
+        // Create tr
+        const tr = document.createElement('tr');
+        tr.classList.add('fade-in');
+
+        // Add td
+        data.forEach(div => {
+            const td = document.createElement('td');
+            td.appendChild(div);
+            tr.appendChild(td);
+        });
+
+        // Define tbody
         const tbody = event.target.closest('tbody');
-        const rows = tbody.querySelectorAll('tr');
-        const lastRow = rows[rows.length - 2];
-        const newRow = lastRow.cloneNode(true);
-        newRow.classList.add('fade-in');
 
-        tbody.insertBefore(newRow, tbody.lastElementChild);
+        // Add td "Delete"
+        const del = tbody.querySelector('tr > td').cloneNode(true);
+        tr.insertBefore(del, tr.firstChild);
+
+        // Add tr
+        tbody.insertBefore(tr, tbody.lastElementChild);
+
+        this.indexValue++;
     }
 
     delete(event) {
