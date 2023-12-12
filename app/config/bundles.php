0cc7846672ab1e362b6dfab38e7722e5289b7497
Vadim Semeniuc <semeniuc@int.pl>
2023-12-12 11:36:34 +0100

    remove ux live components

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -16,6 +16,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
-    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
-    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
