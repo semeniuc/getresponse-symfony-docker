610ffd122e01f312fbdf6fa5941c0c0df9b68e60
Vadim Semeniuc <semeniuc@int.pl>
2023-12-13 17:19:55 +0100

    migration test to work version

---
+++ app/assets/controllers/field-collection_controller.js
@@ -0,0 +1,54 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ['entity', 'bitrix', 'getresponse']
+
+    static values = {
+        index    : Number,
+        prototype: String,
+    }
+
+    add(event) {
+        event.preventDefault();
+        
+        // Generation data
+        const generation =  document.createElement('section');
+        generation.innerHTML = this.prototypeValue.replace(/__name__/g, this.indexValue);
+        
+        // Get data
+        const data = Array.from(generation.querySelectorAll('div > div'));
+
+        // Create tr
+        const tr = document.createElement('tr');
+        tr.classList.add('fade-in');
+
+        // Add td
+        data.forEach(div => {
+            const td = document.createElement('td');
+            td.appendChild(div);
+            tr.appendChild(td);
+        });
+
+        // Define tbody
+        const tbody = event.target.closest('tbody');
+
+        // Add td "Delete"
+        const del = tbody.querySelector('tr > td').cloneNode(true);
+        tr.insertBefore(del, tr.firstChild);
+
+        // Add tr
+        tbody.insertBefore(tr, tbody.lastElementChild);
+        this.indexValue++;
+    }
+
+    delete(event) {
+        event.preventDefault();
+
+        const tr = event.target.closest('tr');
+        tr.classList.add('fade-out');
+
+        setTimeout(() => {
+            tr.remove();
+        }, 500);
+    }
+}
