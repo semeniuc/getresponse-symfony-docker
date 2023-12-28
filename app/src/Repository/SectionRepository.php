483be307857fa6c9f67510babef27e6137e11af1
Vadim Semeniuc <semeniuc@int.pl>
2023-12-28 15:55:25 +0100

    prepare SectionRepository

--- app/src/Repository/SectionRepository.php
+++ app/src/Repository/SectionRepository.php
@@ -3,17 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Section;
+use App\Entity\Client;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Section>
- *
- * @method Section|null find($id, $lockMode = null, $lockVersion = null)
- * @method Section|null findOneBy(array $criteria, array $orderBy = null)
- * @method Section[]    findAll()
- * @method Section[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
 class SectionRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -21,28 +14,43 @@ class SectionRepository extends ServiceEntityRepository
         parent::__construct($registry, Section::class);
     }
 
-//    /**
-//     * @return Section[] Returns an array of Section objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function get(string $memberId): ?Section
+    {
+        return $this->findOneBy(['memberId' => $memberId]);
+    }
 
-//    public function findOneBySomeField($value): ?Section
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function add(Client $client, ?string $listId, ?string $pipelineId): bool
+    {
+        $record = new Section();
+
+        $record->setClient($client);
+        $record->setListId($listId);
+        $record->setPipelineId($pipelineId);
+        $record->setExecutedAt(new \DateTimeImmutable('now', new \DateTimeZone('Europe/Warsaw')));
+
+        $this->getEntityManager()->persist($record);
+        $this->getEntityManager()->flush();
+
+        return true;
+    }
+
+    public function upd(int $id, ?string $listId, ?string $pipelineId): bool
+    {
+        $record = $this->find($id);
+        $record->setListId($listId);
+        $record->setPipelineId($pipelineId);
+        $record->setExecutedAt(new \DateTimeImmutable('now', new \DateTimeZone('Europe/Warsaw')));
+
+        $this->getEntityManager()->flush();
+
+        return true;
+    }
+
+    public function del(int $id): bool
+    {
+        $record = $this->find($id);
+        $record->remove();
+
+        return true;
+    }
 }
